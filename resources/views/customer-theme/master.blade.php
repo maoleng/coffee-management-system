f165f8bd558d8bc565c4ac25521219a7f9bb0485
Bùi Hữu Lộc <feature451@gmail.com>
2023-04-01 22:22:21 +0700

    fix: rename menu to header

--- resources/views/customer-theme/master.blade.php
+++ resources/views/customer-theme/master.blade.php
@@ -2,7 +2,7 @@
 <html class="no-js" lang="en">
 @include('customer-theme.head_tag')
 <body>
-@include('customer-theme.menu')
+@include('customer-theme.header')
 @yield('content')
 @include('customer-theme.footer')
 @include('customer-theme.script')
